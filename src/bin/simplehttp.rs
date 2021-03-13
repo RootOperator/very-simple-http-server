78d54d05fffd22eea32b67ddc65c305ae59f8ad7
RootOperator <20143055+RootOperator@users.noreply.github.com>
2021-03-13 10:36:31 +0100

    quality of life changes when only changing port

--- src/bin/simplehttp.rs
+++ src/bin/simplehttp.rs
@@ -6,7 +6,7 @@ use std::path::Path;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 4 && args.len() != 2 {
+    if args.len() < 4 && args.len() != 2 && args.len() != 3 {
         eprintln!("not enough arguments provided");
         process::exit(1);
     }
@@ -14,7 +14,9 @@ fn main() {
     let query: String = args[1].clone();
 
     let (host, port) = if args.len() == 2 {
-        (String::from("127.0.0.1"), 8000)
+        ("127.0.0.1".to_string(), 8000)
+    } else if args.len() == 3 {
+        ("127.0.0.1".to_string(), args[2].clone().parse::<i16>().unwrap())
     } else {
         (args[2].clone(), args[3].clone().parse::<i16>().unwrap())
     };
